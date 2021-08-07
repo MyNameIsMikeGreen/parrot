df2f8b0ef5b3be9f7322b47789de58a4522b360e
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-08-07 10:55:02 +0100

    Remove console.log() calls

--- src/body_components/blogPost.js
+++ src/body_components/blogPost.js
@@ -5,14 +5,12 @@ import {useParams} from "react-router-dom";
 function BlogPost() {
     let { title } = useParams();
     const markdownUrl = "https://raw.githubusercontent.com/MyNameIsMikeGreen/blog/master/posts/" + title + ".md";
-    console.log(markdownUrl);
     const [markdown, setMarkdown] = React.useState('Fetching blog post...');
 
     React.useEffect(() => {
         const fetchMarkdown = async () => {
             const response = await fetch(markdownUrl);
             const text = await response.text();
-            console.log(text)
             setMarkdown(text);
         };
         fetchMarkdown();
