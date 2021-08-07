b99f6468899b7751c75c56030087bd392dc70c19
MyNameIsMikeGreen <MyNameIsMikeGreen@outlook.com>
2021-08-07 10:46:09 +0100

    Test blog posts

--- src/__tests__/Blog.test.js
+++ src/__tests__/Blog.test.js
@@ -2,22 +2,22 @@ import {findByTestId, render, screen} from '@testing-library/react';
 import Blog from "../body_components/blog";
 
 test('loading message displayed when searching for posts', () => {
-  render(<Blog />);
+    render(<Blog/>);
 
-  expect(screen.getByText("Fetching list of blog posts...")).toBeInTheDocument()
+    expect(screen.getByText("Fetching list of blog posts...")).toBeInTheDocument()
 });
 
 test('contains list of links to GitHub blog repo once searching has completed', async () => {
-  render(<Blog />);
+    render(<Blog/>);
 
-  // TODO: Mock the HTTP response
-  const blogPostListDiv = await findByTestId(document, "parrot-blog-post-list");
-  expect(blogPostListDiv.children.length).toBeGreaterThan(0);
+    // TODO: Mock the HTTP response
+    const blogPostListDiv = await findByTestId(document, "parrot-blog-post-list");
+    expect(blogPostListDiv.children.length).toBeGreaterThan(0);
 
-  for (let i = 0; i < blogPostListDiv.children.length; i++) {
-    const blogPostLink = blogPostListDiv.children[i].firstChild.firstChild;
-    expect(blogPostLink).toHaveAttribute("href", expect.stringMatching(/^\/blog\/[A-Za-z]+$/))
-  }
+    for (let i = 0; i < blogPostListDiv.children.length; i++) {
+        const blogPostLink = blogPostListDiv.children[i].firstChild.firstChild;
+        expect(blogPostLink).toHaveAttribute("href", expect.stringMatching(/^\/blog\/[A-Za-z]+$/))
+    }
 
 });
 
