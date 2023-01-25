3914ac2318d759b19bbe8169603ab4dd91f02d22
Mike Green <8007279+MyNameIsMikeGreen@users.noreply.github.com>
2023-01-25 16:00:12 +0000

    Add image support

--- src/body_components/blogPost.js
+++ src/body_components/blogPost.js
@@ -4,25 +4,24 @@ import {useParams} from "react-router-dom";
 
 function BlogPost() {
     let {title} = useParams();
-    const markdownUrl = "https://raw.githubusercontent.com/MyNameIsMikeGreen/blog/master/posts/" + title + ".md";
-    const githubUrl = "https://github.com/MyNameIsMikeGreen/blog/blob/master/posts/" + title + ".md";
+    const githubRawBaseUrl = "https://raw.githubusercontent.com/MyNameIsMikeGreen/blog/master/posts/";
+    const githubUiBaseUrl = "https://github.com/MyNameIsMikeGreen/blog/blob/master/posts/";
     const [markdown, setMarkdown] = React.useState('Fetching blog post...');
 
     React.useEffect(() => {
         const fetchMarkdown = async () => {
-            const response = await fetch(markdownUrl);
+            const response = await fetch(githubRawBaseUrl + title + ".md");
             const text = await response.text();
             setMarkdown(text);
         };
         fetchMarkdown();
     });
-
     return (
         <div id="parrot-blog-post-wrapper">
             <div id="parrot-blog-post">
-                <ReactMarkdown>{markdown}</ReactMarkdown>
+                <ReactMarkdown transformImageUri={uri => githubRawBaseUrl + uri}>{markdown}</ReactMarkdown>
             </div>
-            <p><a href={githubUrl}>View this on GitHub</a></p>
+            <p><a href={githubUiBaseUrl + title + ".md"}>View this on GitHub</a></p>
         </div>
     );
 }
