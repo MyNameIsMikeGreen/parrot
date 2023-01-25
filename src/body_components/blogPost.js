84bcfbde6abbdf560c4fbb35ad69ecc6c3f5a816
Mike Green <8007279+MyNameIsMikeGreen@users.noreply.github.com>
2023-01-25 15:47:21 +0000

    Add GitHub link to blog posts

--- src/body_components/blogPost.js
+++ src/body_components/blogPost.js
@@ -3,8 +3,9 @@ import ReactMarkdown from "react-markdown";
 import {useParams} from "react-router-dom";
 
 function BlogPost() {
-    let { title } = useParams();
+    let {title} = useParams();
     const markdownUrl = "https://raw.githubusercontent.com/MyNameIsMikeGreen/blog/master/posts/" + title + ".md";
+    const githubUrl = "https://github.com/MyNameIsMikeGreen/blog/blob/master/posts/" + title + ".md";
     const [markdown, setMarkdown] = React.useState('Fetching blog post...');
 
     React.useEffect(() => {
@@ -17,8 +18,11 @@ function BlogPost() {
     });
 
     return (
-        <div id="parrot-blog-post">
-            <ReactMarkdown>{markdown}</ReactMarkdown>
+        <div id="parrot-blog-post-wrapper">
+            <div id="parrot-blog-post">
+                <ReactMarkdown>{markdown}</ReactMarkdown>
+            </div>
+            <p><a href={githubUrl}>View this on GitHub</a></p>
         </div>
     );
 }
