ebb75e46ef7b608c91669b60f802f2bc4e2e2c4c
Mike Green <8007279+MyNameIsMikeGreen@users.noreply.github.com>
2023-11-19 13:05:04 +0000

    Added private links (#135)
    * Add pelican and parrot
    
    * Private links are red
    
    * Simplify warning
    
    * Adjust private font size
    
    * Pelican logo
    
    * Crop Pelican logo

--- src/body_components/Links.js
+++ src/body_components/Links.js
@@ -1,6 +1,7 @@
 import githubLogo from "../media/GitHub_Logo.png";
 import platypusLogo from "../media/Platypus_Logo.png";
 import linkedInLogo from "../media/LinkedIn_Logo.png";
+import pelicanLogo from "../media/Pelican_Logo.png";
 import * as React from "react";
 
 export default class Links extends React.Component {
@@ -22,6 +23,24 @@ export default class Links extends React.Component {
                         <img src={linkedInLogo} alt="LinkedIn Logo"/>
                         <div className="parrot-outlink-main-text">LinkedIn</div>
                         <div className="parrot-outlink-additional-text">Networking</div>
+                    </a>
+                </div>
+
+                <div className="parrot-outlink">
+                    <a href="http://pi:8001">
+                        <img src={platypusLogo} alt="Platypus Logo"/>
+                        <div className="parrot-outlink-main-text">Platypus</div>
+                        <div className="parrot-outlink-additional-text">Recipes</div>
+                        <div className="parrot-outlink-private-network-text">Private Network Only</div>
+                    </a>
+                </div>
+
+                <div className="parrot-outlink">
+                    <a href="http://pi:8000">
+                        <img src={pelicanLogo} alt="Pelican Logo"/>
+                        <div className="parrot-outlink-main-text">Pelican</div>
+                        <div className="parrot-outlink-additional-text">Media Server Control</div>
+                        <div className="parrot-outlink-private-network-text">Private Network Only</div>
                     </a>
                 </div>
 
